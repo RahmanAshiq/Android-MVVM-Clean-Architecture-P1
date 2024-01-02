0853f9a43a80c3c0a8e1fee5d2383fa484976c54
Ashiqur Rahman <ashiq_jb@yahoo.com>
2024-01-02 16:59:12 +0600

    AddedAllBasicDependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "com.example.mvvmcap1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mvvmcap1"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -27,20 +28,68 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+    dataBinding {
+        enable = true
     }
 }
 
 dependencies {
+    val lifecycle_version = "2.6.2"
+    val arch_version = "2.2.0"
+    val room_version = "2.6.1"
+    val coroutine_version = "1.8.0-RC2"
+    val dagger_version = "2.50"
+    val retrofit_version = "2.9.0"
+    val interceptor_version = "4.12.0"
+    val glide_version = "4.16.0"
 
-    implementation("androidx.core:core-ktx:1.9.0")
+
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // ViewModel - https://developer.android.com/jetpack/androidx/releases/lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    // Saved state module for ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version")
+    // Annotation processor
+    kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+
+    // Room - https://developer.android.com/jetpack/androidx/releases/room#kts
+    implementation("androidx.room:room-runtime:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    //Coroutine - https://github.com/Kotlin/kotlinx.coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutine_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutine_version")
+
+    //Dagger - https://github.com/google/dagger
+    implementation("com.google.dagger:dagger:$dagger_version")
+    kapt("com.google.dagger:dagger-compiler:$dagger_version")
+
+    //Retrofit - https://github.com/square/retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    //Gson library - https://square.github.io/retrofit/
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    //Logging interceptor - https://github.com/square/okhttp/tree/master/okhttp-logging-interceptor
+    implementation("com.squareup.okhttp3:logging-interceptor:$interceptor_version")
+
+    //Glide - https://github.com/bumptech/glide & https://bumptech.github.io/glide/doc/download-setup.html
+    implementation("com.github.bumptech.glide:glide:$glide_version")
+    kapt("com.github.bumptech.glide:compiler:$glide_version")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
