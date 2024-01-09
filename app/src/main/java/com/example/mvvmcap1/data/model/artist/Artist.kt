a9f9f21236c2478c9c2e3c23e0feedbabb1f27fa
Ashiqur Rahman <ashiq_jb@yahoo.com>
2024-01-09 11:15:01 +0600

    deletedUnwantedClassAndReference

--- app/src/main/java/com/example/mvvmcap1/data/model/artist/Artist.kt
+++ app/src/main/java/com/example/mvvmcap1/data/model/artist/Artist.kt
@@ -6,8 +6,6 @@ import com.google.gson.annotations.SerializedName
 data class Artist(
     @SerializedName("id")
     val id: Int,
-    @SerializedName("known_for")
-    val knownFor: List<KnownFor>?,
     @SerializedName("name")
     val name: String?,
     @SerializedName("popularity")
